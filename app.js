bfb21fb834e374a0e825f3953c10340916517583
d3gerv <d3gerv23@gmail.com>
2020-02-16 17:52:49 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    alert("Your form has been submitted");
+}
+function cancelClear() {
+    document.getElementById("eventTitle").value = "";
+    document.getElementById("ddate").value = "";
+    document.getElementById("pdate").value = "";
+    document.getElementById("edate").value = "";
+}  
