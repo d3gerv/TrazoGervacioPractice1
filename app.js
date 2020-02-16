87449604a192bc9b0b565412b8c715aca85a669b
d3gerv <d3gerv23@gmail.com>
2020-02-16 23:51:26 +0800

    2ndcommit

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 function myFunction() {
     alert("Your form has been submitted");
+    document.getElementById("event").value = "";
+    document.getElementById("eventTitle").value = "";
+    document.getElementById("ddate").value = "";
+    document.getElementById("pdate").value = "";
+    document.getElementById("edate").value = "";
 }
 function cancelClear() {
+    alert("Your form has been canceled")
+    document.getElementById("event").value = "";
     document.getElementById("eventTitle").value = "";
     document.getElementById("ddate").value = "";
     document.getElementById("pdate").value = "";
