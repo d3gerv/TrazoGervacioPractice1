1ac43c6fefee7e53455e0c2945d245b80b834130
d3gerv <d3gerv23@gmail.com>
2020-02-17 23:53:53 +0800

    3rd commit

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ function myFunction() {
     document.getElementById("ddate").value = "";
     document.getElementById("pdate").value = "";
     document.getElementById("edate").value = "";
+    
+        
 }
 function cancelClear() {
     alert("Your form has been canceled")
@@ -14,3 +16,12 @@ function cancelClear() {
     document.getElementById("pdate").value = "";
     document.getElementById("edate").value = "";
 }  
+function errorDate(){
+    let firstDate = document.getElementById("ddate").value;
+    let lastDate = document.getElementById("edate").value;
+
+   if(firstDate > lastDate){
+        alert("Set Date is not proper")
+        document.getElementById("edate").value = "";
+   }
+}
